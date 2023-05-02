f493010d7204823cdb1939633bc36cdd9ffec861
Vish-was <vshwsnahar3@gmail.com>
2023-05-02 17:53:32 +0530

    Added JWT for athentications

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/auth/login', to: 'authentication#login'
+  resources :users, param: :_username
   resources :short_links, only: [:create, :show]
 end
